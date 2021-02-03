67b1f6dbd905babdcfb5765668f2f951b1457421
Mark Davydov <mark@monday.com>
2021-02-03 12:40:41 +0200

    Added html template executor

--- main.go
+++ main.go
@@ -2,28 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
 }
 
-func (u *User) getAllInfo() string {
-	return fmt.Sprintf("User name is: %s. He is %d and he has money %d", u.name, u.age, u.money)
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money %d", u.Name, u.Age, u.Money)
 }
 
 func (u *User) setName(name string) {
-	u.name = name
+	u.Name = name
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-	bob := User{name: "Bob", age: 25, money: -50, avg_grades: 84.5, happiness: 0.8}
-	bob.setName("Not Bob")
-	fmt.Fprintf(w, bob.getAllInfo())
+	bob := User{Name: "Bob", Age: 25, Money: -50, Avg_grades: 84.5, Happiness: 0.8}
+	//bob.setName("Not Bob")
+	//fmt.Fprintf(w, bob.getAllInfo())
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, bob)
 }
 
 func about_page(w http.ResponseWriter, r *http.Request) {
